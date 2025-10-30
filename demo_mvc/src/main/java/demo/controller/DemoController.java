f21e78494e847ec8fbd47742edb9a4ccc9203a94
Jordan <Jordanabid@gmail.com>
2025-10-30 16:44:24 +0100

    Spring MVC - Jour 1 - @RequestMapping

--- demo_mvc/src/main/java/demo/controller/DemoController.java
+++ demo_mvc/src/main/java/demo/controller/DemoController.java
@@ -1,9 +1,30 @@
 package demo.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class DemoController {
 
+
+	@RequestMapping(value="/home")
+	public String accueil() 
+	{
+		return "/WEB-INF/demo.jsp";
+	}
+
+	@RequestMapping(value="/demo",method = RequestMethod.POST)
+	public String recevoirFormAccueil(String prenom,int age,Model model,HttpSession session) 
+	{
+		session.setAttribute("loginSession", "tpt");
+		System.out.println(prenom);
+		System.out.println(age);
+		model.addAttribute("maVariable", prenom+"-"+age);
+		return "/WEB-INF/apresForm.jsp";
+	}
 
 }
---
+++ demo_mvc/src/main/webapp/WEB-INF/demo.jsp
@@ -0,0 +1,22 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+
+
+
+<form action="demo" method="POST">
+	<input type="text" name="prenom" placeholder="Saisir prenom"><br>
+	<input type="number" name="age" placeholder="Saisir age"><br>
+	<input type="submit" value="GO">
+</form>
+
+
+
+</body>
+</html>
