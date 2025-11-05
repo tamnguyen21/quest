9b47156d381e8f0e16459c7e5e597574e79bdf1b
Jérémy <jeremy.perrouault@ascadis.fr>
2025-11-05 10:08:11 +0000

    JOUR 1 - INTRO SPRING BOOT

---
+++ formation-boot/src/test/java/fr/formation/formation_boot/FormationBootApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.formation.formation_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormationBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
