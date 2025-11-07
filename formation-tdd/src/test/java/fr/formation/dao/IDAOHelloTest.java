410d9b6d6779abcd38e656861e477223fd295944
Jérémy <jeremy.perrouault@ascadis.fr>
2025-11-07 13:38:20 +0000

    JOUR 1 - DATA JPA TEST AVEC DONNEES

--- formation-tdd/src/test/java/fr/formation/dao/IDAOHelloTest.java
+++ formation-tdd/src/test/java/fr/formation/dao/IDAOHelloTest.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.Sql.ExecutionPhase;
 
 import fr.formation.model.Hello;
 
@@ -16,6 +18,8 @@ public class IDAOHelloTest {
     private IDAOHello dao;
 
     @Test
+    @Sql(scripts = "classpath:/scripts/create-hello.sql")
+    @Sql(scripts = "classpath:/scripts/remove-hello.sql", executionPhase = ExecutionPhase.AFTER_TEST_METHOD)
     void shouldFindAllByMessageReturnAllValues() {
         // given
 
---
+++ formation-tdd/src/test/resources/scripts/create-hello.sql
@@ -0,0 +1,6 @@
+INSERT INTO hello (message) VALUES
+    ('Bonjour à tous'),
+    ('Bonjour tout le monde'),
+    ('Bonjour à toi'),
+    ('Au revoir.')
+;
