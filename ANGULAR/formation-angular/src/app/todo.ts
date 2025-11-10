f743d54d781963a254bb6e1a05c4e9a7f59d6df4
Jérémy <jeremy.perrouault@ascadis.fr>
2025-11-10 14:08:25 +0000

    JOUR 1 - CLASS TODO

---
+++ ANGULAR/formation-angular/src/app/todo.ts
@@ -0,0 +1,40 @@
+export class Todo {
+    constructor(
+        private _id: number,
+        private _title: string,
+        private _completed: boolean,
+        private _userId: number
+    ) { }
+
+    public get id(): number {
+        return this._id;
+    }
+
+    public set id(value: number) {
+        this._id = value;
+    }
+
+    public get title(): string {
+        return this._title;
+    }
+
+    public set title(value: string) {
+        this._title = value;
+    }
+
+    public get completed(): boolean {
+        return this._completed;
+    }
+
+    public set completed(value: boolean) {
+        this._completed = value;
+    }
+
+    public get userId(): number {
+        return this._userId;
+    }
+
+    public set userId(value: number) {
+        this._userId = value;
+    }
+}
