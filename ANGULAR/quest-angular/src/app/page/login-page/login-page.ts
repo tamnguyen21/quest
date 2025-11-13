b9904db5fe09d086c22235b858802e091c9179f3
Jérémy <jeremy.perrouault@ascadis.fr>
2025-11-13 15:58:38 +0000

    JOUR 3 - REACTIVE FORMS - VALIDATORS

--- ANGULAR/quest-angular/src/app/page/login-page/login-page.ts
+++ ANGULAR/quest-angular/src/app/page/login-page/login-page.ts
@@ -1,22 +1,26 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { AuthRequestDto } from '../../dto/auth-request-dto';
 import { AuthService } from '../../service/auth-service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login-page',
-  imports: [ ReactiveFormsModule ],
+  imports: [ CommonModule, ReactiveFormsModule ],
   templateUrl: './login-page.html',
   styleUrl: './login-page.css',
 })
 export class LoginPage implements OnInit {
   protected userForm!: FormGroup;
+  protected usernameCtrl!: FormControl;
 
   constructor(private authService: AuthService, private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.usernameCtrl = this.formBuilder.control('', Validators.required);
+
     this.userForm = this.formBuilder.group({
-      username: this.formBuilder.control(''),
+      username: this.usernameCtrl,
       password: this.formBuilder.control('')
     });
   }
