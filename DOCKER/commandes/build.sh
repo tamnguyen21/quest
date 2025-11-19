7e0ce0c4bb30d2cc7dddb904225e3aeb398765d9
Jérémy <jeremy.perrouault@ascadis.fr>
2025-11-19 17:56:44 +0000

    JOUR 1 - COMMANDES BUILD

---
+++ DOCKER/commandes/build.sh
@@ -0,0 +1,8 @@
+# Builder une image à partir de Dockerfile (se placer dans le répertoire où ce trouve le Dockerfile)
+docker build -t nom_image:tag_image .
+
+# Builder une image à partir de Dockerfile ("C:/chemin/vers/contenu" est le répertoire où se trouve le Dockerfile)
+docker build -t nom_image:tag_image C:/chemin/vers/contenu
+
+# Exemple
+docker build -t ajc/quest:boot .
