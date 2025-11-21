2ac1426905fb4490556809a374253928a6030b22
theblouch <thibaud.reigner@gmail.com>
2025-11-21 16:47:59 +0100

    Ajout des logs dans OrdinateurRestController et OrdinateurService

--- quest-boot/src/main/java/quest/rest/OrdinateurRestController.java
+++ quest-boot/src/main/java/quest/rest/OrdinateurRestController.java
@@ -2,6 +2,8 @@ package quest.rest;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.fasterxml.jackson.annotation.JsonView;
 
+import quest.config.SecurityConfig;
 import quest.model.Ordinateur;
 import quest.service.OrdinateurService;
 import quest.view.Views;
@@ -22,23 +25,29 @@ import quest.view.Views;
 @RequestMapping("/api/ordinateur")
 public class OrdinateurRestController {
 
+	private final static Logger log = LoggerFactory.getLogger(SecurityConfig.class);
+
 	@Autowired
 	OrdinateurService ordinateurSrv;
 
 	@JsonView(Views.Ordinateur.class)
 	@GetMapping
 	public List<Ordinateur> allOrdinateurs() {
+		log.info("Lancement de la fonction allOrdinateurs");
 		return ordinateurSrv.getAll();
 	}
 
 	@JsonView(Views.Ordinateur.class)
 	@GetMapping("/{id}")
 	public Ordinateur ficheOrdinateur(@PathVariable Integer id, Ordinateur ordinateur) {
+		log.info("Lancement de la fonction ficheOrdinateur avec l'id {}", id);
 		return ordinateurSrv.getById(id);
 	}
 
 	@PostMapping
 	public Ordinateur ajoutOrdinateur(@RequestBody Ordinateur ordinateur) {
+		log.info("Lancement de la fonction ajoutOrdinateur avec les données : id = {}, marque = {}, RAM = {}",
+				ordinateur.getId(), ordinateur.getMarque(), ordinateur.getRam());
 		return ordinateurSrv.create(ordinateur);
 	}
 
@@ -46,12 +55,16 @@ public class OrdinateurRestController {
 	@PutMapping("/{id}")
 	public Ordinateur modifierOrdinateur(@PathVariable Integer id, @RequestBody Ordinateur ordinateur) {
 		ordinateur.setId(id);
+		log.info(
+				"Lancement de la fonction modifierOrdinateur d'id {} avec les données : id = {}, marque = {}, RAM = {}",
+				id, ordinateur.getId(), ordinateur.getMarque(), ordinateur.getRam());
 		return (Ordinateur) ordinateurSrv.update(ordinateur);
 	}
 
 	@JsonView(Views.Ordinateur.class)
 	@DeleteMapping("/{id}")
 	public void supprimerOrdinateur(@PathVariable Integer id) {
+		log.info("Lancement de lafonction supprimerOrdinateur d'id {}", id);
 		ordinateurSrv.deleteById(id);
 	}
 
--- quest-boot/src/main/java/quest/service/OrdinateurService.java
+++ quest-boot/src/main/java/quest/service/OrdinateurService.java
@@ -3,9 +3,12 @@ package quest.service;
 import java.util.List;
 import java.util.Optional;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import quest.config.SecurityConfig;
 import quest.dao.IDAOOrdinateur;
 import quest.model.Ordinateur;
 
@@ -15,37 +18,52 @@ public class OrdinateurService {
 	@Autowired
 	IDAOOrdinateur daoOrdinateur;
 
-	public Ordinateur getById(Integer id)
-	{
-		/*Optional <Ordinateur> opt = daoOrdinateur.findById(id);
-		if(opt.isEmpty()) {return null;}
-		else {return opt.get();}*/
-		return daoOrdinateur.findById(id).orElse(null);
+	private final static Logger log = LoggerFactory.getLogger(SecurityConfig.class);
+
+	public Ordinateur getById(Integer id) {
+		/*
+		 * Optional <Ordinateur> opt = daoOrdinateur.findById(id);
+		 * if(opt.isEmpty()) {return null;}
+		 * else {return opt.get();}
+		 */
+		log.info("Lancement de la fonction getById avec id : {}", id);
+		Optional<Ordinateur> res = daoOrdinateur.findById(id);
+		if (res.isEmpty()) {
+			log.error("Ordinateur d'id {} non trouvé", id);
+			return null;
+		} else {
+			log.info("Ordinateur d'id {} trouvé", id);
+			return res.get();
+		}
 	}
 
-	public List<Ordinateur> getAll()
-	{
+	public List<Ordinateur> getAll() {
+		log.info("Lancement de la fonction getAll");
 		return daoOrdinateur.findAll();
 	}
 
-	public Ordinateur create(Ordinateur ordinateur) 
-	{
+	public Ordinateur create(Ordinateur ordinateur) {
+		log.info("Lancement de la fonction create avec id : {}, marque : {}, RAM : {}",
+				ordinateur.getId(), ordinateur.getMarque(), ordinateur.getRam());
 		return daoOrdinateur.save(ordinateur);
 	}
 
-	public Ordinateur update(Ordinateur ordinateur) 
-	{
+	public Ordinateur update(Ordinateur ordinateur) {
+		log.info("Lancement de la fonction update avec id : {}, marque : {}, RAM : {}",
+				ordinateur.getId(), ordinateur.getMarque(), ordinateur.getRam());
 		return daoOrdinateur.save(ordinateur);
 	}
 
-	public void deleteById(Integer id) 
-	{
+	public void deleteById(Integer id) {
+		log.info("Lancement de la fonction create avec id : {}", id);
 		daoOrdinateur.deleteById(id);
 	}
 
-	public void delete(Ordinateur ordinateur)
-	{
+	public void delete(Ordinateur ordinateur) {
+		log.warn("Attention une tentative de delete de l' ordinateur d'id {} va avoir lieu", ordinateur.getId());
+		log.info("Lancement de la fonction delete avec id : {}, marque : {}, RAM : {}",
+				ordinateur.getId(), ordinateur.getMarque(), ordinateur.getRam());
 		daoOrdinateur.delete(ordinateur);
 	}
-	
+
 }
